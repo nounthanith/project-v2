9e8b167ae5047a6139af8269beeb3b65f7cebb2c
NuonThanith <163116657+nounthanith@users.noreply.github.com>
2025-06-24 14:24:53 +0700

    use pixel font

--- javascript/products/getProducts.js
+++ javascript/products/getProducts.js
@@ -70,9 +70,9 @@ function renderProducts(products) {
           <div class="product-date">${formatDate(createdAt)}</div>
           <h3 class="product-title">${name}</h3>
           <div class="product-price mb-2">$${parseFloat(price).toFixed(2)}</div>
-          <div class="product-rating mb-2 text-warning d-flex justify-content-between">
-            <div style="font-size:19px">${"★".repeat(Math.floor(rating))}${"☆".repeat(5 - Math.floor(rating))}</div>
-            <p style="background-color:hotpink;padding:0px 15px;" class="text-dark rounded-pill">${rating}</p>
+          <div class="product-rating mb-2 d-flex justify-content-between align-items-center">
+            <div style="font-size:19px;color:#fcbf49;">${"★".repeat(Math.floor(rating))}${"☆".repeat(5 - Math.floor(rating))}</div>
+            <p style="background-color:#fcbf49;padding:0px 15px;border: solid 1px black !important; box-shadow: 2px 2px black;font-family: 'Press Start 2P', cursive;" class="text-dark">${rating}</p>
           </div>
         </div>
       </div>
